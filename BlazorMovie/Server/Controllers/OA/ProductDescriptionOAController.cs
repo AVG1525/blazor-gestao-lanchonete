918bac3177e7524d1723b56728db6f819e63d562
Arrow <aronvinicius15@gmail.com>
2020-11-03 20:59:42 -0300

    resolvendo problemas tecnicos

--- BlazorMovie/Server/Controllers/OA/ProductDescriptionOAController.cs
+++ BlazorMovie/Server/Controllers/OA/ProductDescriptionOAController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BlazorMovie.Server.Infra;
+﻿using BlazorMovie.Server.Infra;
 using BlazorMovie.Shared.Model.OA;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace BlazorMovie.Server.Controllers.OA
 {
--- BlazorMovie/Server/Controllers/OA/SaleOAController.cs
+++ BlazorMovie/Server/Controllers/OA/SaleOAController.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using BlazorMovie.Server.Infra;
+using BlazorMovie.Shared.Model.OA;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BlazorMovie.Server.Infra;
-using BlazorMovie.Shared.Model.OA;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace BlazorMovie.Server.Controllers.OA
 {
