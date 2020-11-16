2140f685eb374f90bf0d5a792a9146ba8f4052d7
Arrow <aronvinicius15@gmail.com>
2020-11-16 08:44:59 -0300

    Adicionando classe para inserção dos dados

---
+++ BlazorMovie/Shared/Model/ModelBuilderExtensions/DataSeeding.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorMovie.Shared.Model.ModelBuilderExtensions
+{
+    public static class DataSeeding
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Post>().HasData(
+            //    new { BlogId = 1, PostId = 2, Title = "Second post", Content = "Test 2" });
+        }
+    }
+}
